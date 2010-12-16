e0e0e2cc83853c587ecef45f04973dda94a5f95d
Andi Kleen <ak@linux.intel.com>
2010-12-17 00:50:58 +0100

    x86: Fix sys_call_table type in asm/syscall.h v2
    Make the sys_call_table type defined in asm/syscall.h match
    the definition in syscall_64.c
    
    v2: include asm/syscall.h in syscall_64.c too. I left uml alone
    because it doesn't have an syscall.h on its own and including
    the native one leads to other errors.
    Cc: x86@kernel.org
    Signed-off-by: Andi Kleen <ak@linux.intel.com>

--- arch/x86/include/asm/syscall.h
+++ arch/x86/include/asm/syscall.h
@@ -16,7 +16,8 @@
 #include <linux/sched.h>
 #include <linux/err.h>
 
-extern const unsigned long sys_call_table[];
+typedef void (*sys_call_ptr_t)(void);
+extern const sys_call_ptr_t sys_call_table[];
 
 /*
  * Only the low 32 bits of orig_ax are meaningful, so we return int.
