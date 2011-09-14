1e878e6d59474dcd20239ad6faf02b5bd3894a44
Andi Kleen <ak@linux.intel.com>
2011-09-15 00:53:13 +0200

    fix vsyscall merge

--- arch/x86/kernel/vsyscall_64.c
+++ arch/x86/kernel/vsyscall_64.c
@@ -269,14 +269,7 @@ bool emulate_vsyscall(struct pt_regs *regs, unsigned long address)
 		return true;  /* Don't emulate the ret. */
 	}
 
-<<<<<<< HEAD
 	regs->ax = ret;
-=======
-long __vsyscall(3) venosys_1(void)
-{
-	return -ENOSYS;
-}
->>>>>>> add visible to vsyscall and make not static
 
 	/* Emulate a ret instruction. */
 	regs->ip = caller;
