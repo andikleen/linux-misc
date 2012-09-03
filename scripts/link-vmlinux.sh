f82233a8b0c0561481632daefc6536d115beca10
Andi Kleen <ak@linux.intel.com>
2012-09-03 21:18:23 +0200

    Kbuild, lto: Add Link Time Optimization support
    With LTO gcc will do whole program optimizations for
    the whole kernel and each module. This increases compile time,
    but can generate faster and smaller code and allows
    the compiler to do global checking. For example the compiler
    can complain now about type mismatches for symbols between
    different files.
    
    LTO allows gcc to inline functions between different files and
    do various other optimization across the whole binary.
    
    The LTO patches have been used for many years by various
    users, mostly to make their kernel smaller. This version
    has a lot of outdated cruft dropped and doesn't need
    any special tool chain (except for new enough) anymore.
    
    This adds the basic Kbuild plumbing for LTO:
    
    - In Kbuild add a new scripts/Makefile.lto that checks
    the tool chain and does all the necessary setup.
    LTO has more requirements on the toolchain than a normal build.
    When anything fails disable LTO
    - Add a new LDFINAL variable that controls the final link
    for vmlinux or module. In this case we call gcc-ld instead
    of ld, to run the LTO step.
    
    - Kconfigs:
    Since LTO with allyesconfig needs more than 4G of memory (~8G)
    and has the potential to makes people's system swap to death.
    I used a nested config that ensures that a simple
    allyesconfig disables LTO. It has to be explicitely
    enabled.
    
    For more information see Documentation/kbuild/lto-build.rst
    
    Thanks to HJ Lu, Joe Mario, Honza Hubicka, Richard Guenther,
    Don Zickus, Changlong Xie, Gleb Schukin, various github contributors,
     who helped with this project (and probably some more who I forgot, sorry)
    
    Signed-off-by: Andi Kleen <ak@linux.intel.com>

--- scripts/link-vmlinux.sh
+++ scripts/link-vmlinux.sh
@@ -56,7 +56,7 @@ modpost_link()
 		${KBUILD_VMLINUX_LIBS}				\
 		--end-group"
 
-	${LD} ${KBUILD_LDFLAGS} -r -o ${1} ${objects}
+	${LDFINAL} ${KBUILD_LDFLAGS} -r ${KBUILD_MODPOST_LDFLAGS} -o ${1} ${objects}
 }
 
 objtool_link()
@@ -92,15 +92,17 @@ vmlinux_link()
 	local objects
 	local strip_debug
 
-	info LD ${output}
+	info LDFINAL ${output}
 
 	# skip output file argument
 	shift
 
 	# The kallsyms linking does not need debug symbols included.
-	# except for LTO because gcc 10 LTO changes the layout of the data segment
-	# with --strip-debug
-	if [ "$output" != "${output#.tmp_vmlinux.kallsyms}" -a -z "$CONFIG_LTO" ] ; then
+	# except for gcc LTO because gcc 10 LTO changes the layout of the
+	# data segment with --strip-debug
+	if [ "$output" != "${output#.tmp_vmlinux.kallsyms}" -a 	\
+	     -z "$CONFIG_LTO"  -a 				\
+	     -n "$CONFIG_CC_IS_GCC" ] ; then
 		strip_debug=-Wl,--strip-debug
 	fi
 
@@ -113,7 +115,7 @@ vmlinux_link()
 			--end-group				\
 			${@}"
 
-		${LD} ${KBUILD_LDFLAGS} ${LDFLAGS_vmlinux}	\
+		${LDFINAL} ${KBUILD_LDFLAGS} ${LDFLAGS_vmlinux}	\
 			${strip_debug#-Wl,}			\
 			-o ${output}				\
 			-T ${lds} ${objects}
@@ -189,7 +191,38 @@ kallsyms()
 	fi
 
 	info KSYMS ${2}
-	${NM} -n ${1} | scripts/kallsyms ${kallsymopt} > ${2}
+	(
+	if [ -n "$CONFIG_LTO" -a -n "$CONFIG_KALLSYMS_SINGLE" -a -n "$CONFIG_CC_IS_GCC" ] &&
+		( ${OBJDUMP} -h ${1} | grep -q gnu\.lto) ; then
+        # workaround for slim LTO gcc-nm not outputing static symbols
+        # http://gcc.gnu.org/PR60016
+        # generate a fake symbol table based on the LTO function sections.
+        # This unfortunately "knows" about the internal LTO file format
+        # and only works for functions
+
+	# read the function names directly from the LTO object
+	objdump -h ${1} |
+		awk '/gnu\.lto_[a-z]/ {
+			gsub(/\.gnu\.lto_/,"");
+			gsub(/\..*/, "");
+			print "0 t " $2
+		     } '
+	# read the non LTO symbols with readelf (which doesn't use the LTO plugin,
+	# so we only get pure ELF symbols)
+	# readelf doesn't handle ar, so we have to expand the objects
+	echo ${1} | sed 's/ /\n/g' | grep built-in.a | while read i ; do
+		${AR} t $i | while read j ; do readelf -s $j ; done
+	done | awk 'NF >= 8 { print "0 t " $8 } '
+	# now handle the objects
+	echo ${1} | sed 's/ /\n/g' | grep '\.o$' | while read i ; do
+		readelf -s $i
+	done | awk 'NF >= 8 {
+	if ($8 !~ /Name|__gnu_lto_slim|\.c(\.[0-9a-f]+)?/) { print "0 t " $8 }
+	}'
+	else
+		${NM} -n ${1}
+	fi
+	) | scripts/kallsyms ${kallsymopt} > ${2}
 }
 
 # Perform one step in kallsyms generation, including temporary linking of
@@ -276,7 +309,7 @@ fi;
 ${MAKE} -f "${srctree}/scripts/Makefile.build" obj=init need-builtin=1
 
 #link vmlinux.o
-info LD vmlinux.o
+info LDFINAL vmlinux.o
 modpost_link vmlinux.o
 objtool_link vmlinux.o
 
@@ -340,6 +373,7 @@ if [ -n "${CONFIG_KALLSYMS}" ]; then
 	fi
 fi
 
+info LDFINAL vmlinux
 vmlinux_link vmlinux "${kallsymso}" ${btf_vmlinux_bin_o}
 
 # fill in BTF IDs
