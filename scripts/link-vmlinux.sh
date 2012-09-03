60606eeb794c307eb951039a78b38a914f87c819
Andi Kleen <ak@linux.intel.com>
2012-09-03 17:26:38 +0200

    Kbuild, lto: Use vmlinux.o to build final vmlinux
    Now that the vmlinux.o contains the final version we can use it
    to build the final vmlinux.o
    
    Preparation for LTO, where we want to do this build step only once
    because it's quite expensive.
    
    Signed-off-by: Andi Kleen <ak@linux.intel.com>

--- scripts/link-vmlinux.sh
+++ scripts/link-vmlinux.sh
@@ -41,7 +41,9 @@ info()
 # ${1} output file
 modpost_link()
 {
-	${LD} ${LDFLAGS} -r -o ${1} ${KBUILD_VMLINUX_INIT}                   \
+	${LD} ${LDFLAGS} ${LDFLAGS_vmlinux} -r                               \
+                -T ${objtree}/${KBUILD_LDS} 			             \
+                -o ${1} ${KBUILD_VMLINUX_INIT}                               \
 		--start-group ${KBUILD_VMLINUX_MAIN} --end-group
 }
 
@@ -54,14 +56,11 @@ vmlinux_link()
 
 	if [ "${SRCARCH}" != "um" ]; then
 		${LD} ${LDFLAGS} ${LDFLAGS_vmlinux} -o ${2}                  \
-			-T ${lds} ${KBUILD_VMLINUX_INIT}                     \
-			--start-group ${KBUILD_VMLINUX_MAIN} --end-group ${1}
+			-T ${lds} vmlinux.o		                     \
+			${1}
 	else
 		${CC} ${CFLAGS_vmlinux} -o ${2}                              \
-			-Wl,-T,${lds} ${KBUILD_VMLINUX_INIT}                 \
-			-Wl,--start-group                                    \
-				 ${KBUILD_VMLINUX_MAIN}                      \
-			-Wl,--end-group                                      \
+			-Wl,-T,${lds} vmlinux.o		                     \
 			-lutil ${1}
 		rm -f linux
 	fi
