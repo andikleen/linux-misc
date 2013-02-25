1cfef5af7d1bddbd87e012e1144c0324195d4f85
Andi Kleen <ak@linux.intel.com>
2013-02-25 02:47:20 +0100

    fix dump_stack asmlinkage for 32bit

--- lib/dump_stack.c
+++ lib/dump_stack.c
@@ -6,7 +6,7 @@
 #include <linux/kernel.h>
 #include <linux/export.h>
 
-void dump_stack(void)
+asmlinkage void dump_stack(void)
 {
 	printk(KERN_NOTICE
 		"This architecture does not implement dump_stack()\n");
