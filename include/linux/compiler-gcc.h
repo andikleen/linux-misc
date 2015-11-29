7f6cb79308aed716dbba22422d474e2f65553b54
Andi Kleen <ak@linux.intel.com>
2015-11-28 16:40:16 -0800

    Allow to build with gcc 6

--- include/linux/compiler-gcc.h
+++ include/linux/compiler-gcc.h
@@ -118,7 +118,11 @@
 #define __gcc_header(x) #x
 #define _gcc_header(x) __gcc_header(linux/compiler-gcc##x.h)
 #define gcc_header(x) _gcc_header(x)
+#if __GNUC__ <= 5
 #include gcc_header(__GNUC__)
+#else
+#include gcc_header(5)
+#endif
 
 #if !defined(__noclone)
 #define __noclone	/* not needed */
