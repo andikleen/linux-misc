c85688d6114515187821cea0c4207a9d78343468
Andi Kleen <ak@linux.intel.com>
2017-04-09 22:10:40 -0700

    lto: Use C version for SYSCALL_ALIAS
    LTO doesn't like the assembler aliasing used for SYSCALL_ALIAS.
    Replace it with C aliasing. Also mark the only user visible.
    
    Signed-off-by: Andi Kleen <ak@linux.intel.com>

--- kernel/time/posix-stubs.c
+++ kernel/time/posix-stubs.c
@@ -24,7 +24,7 @@
 #include <asm/syscall_wrapper.h>
 #endif
 
-asmlinkage long sys_ni_posix_timers(void)
+__visible asmlinkage long sys_ni_posix_timers(void)
 {
 	pr_err_once("process %d (%s) attempted a POSIX timer syscall "
 		    "while CONFIG_POSIX_TIMERS is not set\n",
