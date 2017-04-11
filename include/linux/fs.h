2f6d0be39eac1492568681bb13069641b84d25f3
Andi Kleen <ak@linux.intel.com>
2017-04-11 01:55:19 -0700

    fs: Avoid static variable in linux/fs.h
    linux/fs.h has a initialized static variable kernel_read_file_str. It doesn't
    make much sense to have a static variable in a frequently included
    header file. With LTO -fno-toplevel-reorder gcc is unable to eliminate
    it, which leads to a lot of unnecessary duplicated copies.
    
    Move the static into the scope of the only inline that uses it,
    this tells the compiler enough to not duplicate it. Right now
    the inline is only called from one place, so that is ok. If it was
    called from more places would need to move it somewhere else
    to avoid unnecessary copies.
    
    With LTO this avoids ~100k unnecessary data segment for a x86 defconfig
    build.
    
    Signed-off-by: Andi Kleen <ak@linux.intel.com>

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -2672,12 +2672,12 @@ enum kernel_read_file_id {
 	__kernel_read_file_id(__fid_enumify)
 };
 
-static const char * const kernel_read_file_str[] = {
-	__kernel_read_file_id(__fid_stringify)
-};
-
 static inline const char *kernel_read_file_id_str(enum kernel_read_file_id id)
 {
+	static const char * const kernel_read_file_str[] = {
+		__kernel_read_file_id(__fid_stringify)
+	};
+
 	if ((unsigned)id >= READING_MAX_ID)
 		return kernel_read_file_str[READING_UNKNOWN];
 
