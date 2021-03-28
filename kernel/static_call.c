d6bdeda72a5a723d9e920b780e9e2bf29b2641bb
Andi Kleen <andi@firstfloor.org>
2021-03-28 10:28:16 -0700

    static_call: Make static call functions visible
    Since static call functions are referenced from inline assembler
    they must be __visible and global with gcc LTO. Fix this for the static
    call self tests.
    
    Cc: peterz@infradead.org
    Cc: rostedt@goodmis.org
    Signed-off-by: Andi Kleen <andi@firstfloor.org>

--- kernel/static_call.c
+++ kernel/static_call.c
@@ -440,7 +440,7 @@ early_initcall(static_call_init);
 
 #ifdef CONFIG_STATIC_CALL_SELFTEST
 
-static int func_a(int x)
+int __visible sc_func_a(int x)
 {
 	return x+1;
 }
@@ -450,7 +450,7 @@ static int func_b(int x)
 	return x+2;
 }
 
-DEFINE_STATIC_CALL(sc_selftest, func_a);
+DEFINE_STATIC_CALL(sc_selftest, sc_func_a);
 
 static struct static_call_data {
       int (*func)(int);
@@ -459,7 +459,7 @@ static struct static_call_data {
 } static_call_data [] __initdata = {
       { NULL,   2, 3 },
       { func_b, 2, 4 },
-      { func_a, 2, 3 }
+      { sc_func_a, 2, 3 }
 };
 
 static int __init test_static_call_init(void)
