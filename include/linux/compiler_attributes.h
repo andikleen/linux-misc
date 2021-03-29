125d239695a014bc10e6c745250c205c4e5b849c
Andi Kleen <andi@firstfloor.org>
2021-03-29 08:17:46 -0700

    Compiler attributes, lto: disable __flatten with LTO
    Using __flatten causes a simple gcc 12 LTO build not fit into 16GB
    anymore. Disable flatten with LTO. With gcc 12, the build still does not
    finish linking in 10 minutes, eating 40GB of RAM at that point.
    
    There is an upstream bug about this:
    https://gcc.gnu.org/bugzilla/show_bug.cgi?id=107014
    
    Until this is resolved, simply disable __flatten with LTO.
    
    In the future, instead of this patch, we should likely drop __flatten
    and its only user (pcpu_build_alloc_info()) and use always_inline to all
    functions which shall be inlined there.
    
    Cc: Miguel Ojeda <ojeda@kernel.org>
    Cc: Nick Desaulniers <ndesaulniers@google.com>
    Signed-off-by: Andi Kleen <andi@firstfloor.org>
    Signed-off-by: Martin Liska <mliska@suse.cz>
    Signed-off-by: Jiri Slaby (SUSE) <jirislaby@kernel.org>

--- include/linux/compiler_attributes.h
+++ include/linux/compiler_attributes.h
@@ -229,7 +229,12 @@
  * gcc: https://gcc.gnu.org/onlinedocs/gcc/Common-Function-Attributes.html#Common-Function-Attributes
  * clang: https://clang.llvm.org/docs/AttributeReference.html#flatten
  */
+#ifndef CONFIG_LTO_GCC
 # define __flatten			__attribute__((flatten))
+#else
+/* Causes very large memory use with gcc in LTO mode */
+# define __flatten
+#endif
 
 /*
  * Note the missing underscores.
