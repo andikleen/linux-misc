13b1df9643a5a51da03c76c5d14cade36f1ddda2
Peter Xu <peterx@redhat.com>
2021-04-22 16:42:31 +1000

    userfaultfd/hugetlbfs: fix minor fault page leak
    When uffd-minor enabled, we need to put the page cache before handling the
    userfault in hugetlb_no_page(), otherwise the page refcount got leaked.
    
    This can be reproduced by running userfaultfd selftest with hugetlb_shared
    mode, then cat /proc/meminfo.
    
    Link: https://lkml.kernel.org/r/20210322175132.36659-1-peterx@redhat.com
    Fixes: f2bf15fb0969 ("userfaultfd: add minor fault registration mode")
    Signed-off-by: Peter Xu <peterx@redhat.com>
    Reviewed-by: Mike Kravetz <mike.kravetz@oracle.com>
    Reviewed-by: Axel Rasmussen <axelrasmussen@google.com>
    Cc: Andrea Arcangeli <aarcange@redhat.com>
    Cc: Mike Rapoport <rppt@linux.vnet.ibm.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>

--- mm/hugetlb.c
+++ mm/hugetlb.c
@@ -4609,6 +4609,7 @@ retry:
 		/* Check for page in userfault range. */
 		if (userfaultfd_minor(vma)) {
 			unlock_page(page);
+			put_page(page);
 			ret = hugetlb_handle_userfault(vma, mapping, idx,
 						       flags, haddr,
 						       VM_UFFD_MINOR);
