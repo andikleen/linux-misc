8bd3d802fff27d2819d5258450a04b0b5ce55dfc
Andi Kleen <andi@firstfloor.org>
2021-05-02 07:10:06 -0700

    fixup! x86/lto: Mark 32bit mem{cpy,move,set} __used

--- arch/x86/boot/compressed/string.c
+++ arch/x86/boot/compressed/string.c
@@ -50,7 +50,7 @@ void *memset(void *s, int c, size_t n)
 	return s;
 }
 
-void *memmove(void *dest, const void *src, size_t n)
+__visible __used void *memmove(void *dest, const void *src, size_t n)
 {
 	unsigned char *d = dest;
 	const unsigned char *s = src;
@@ -65,7 +65,7 @@ void *memmove(void *dest, const void *src, size_t n)
 }
 
 /* Detect and warn about potential overlaps, but handle them with memmove. */
-void *memcpy(void *dest, const void *src, size_t n)
+__visible __used void *memcpy(void *dest, const void *src, size_t n)
 {
 	if (dest > src && dest - src < n) {
 		warn("Avoiding potentially unsafe overlapping memcpy()!");
